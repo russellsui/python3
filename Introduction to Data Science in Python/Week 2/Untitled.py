49ca923fcd5a38cd2bfdddb273fbd9d7cba3f2fe
russellsui <qsui@ncsu.edu>
2018-08-26 14:48:51 -0400

    Upload this file

--- Introduction to Data Science in Python/Week 2/Untitled.py
+++ Introduction to Data Science in Python/Week 2/Untitled.py
@@ -21,6 +21,23 @@ purchase_2 = pd.Series({'Name': 'Kevyn',
 purchase_3 = pd.Series({'Name': 'Vinod',
                         'Item Purchased': 'Bird Seed',
                         'Cost': 5.00})
-
+# 在 Series 中，index 是dict中(label,value)中的label，而在dataframe中，index是需要自己赋值的
 df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+# 这就相当于在Dataframe中，列是可以直接用label调用的方式读取的而行是需要用loc函数的
 print(df['Item Purchased'])
+# 也可以对于两个维度同时进行选取，比如
+df.loc['Store 1', 'Cost']
+df.loc['Store 1']['Cost']
+# select some columns from the table using slicing
+df.loc[:,['Name','Cost']]
+# drop some rows using drop function
+df.drop['Store 1']
+# this dropping just changes a copy of the original dataframe so the original dataframe does not actually changes
+copy_df = df.copy()
+copy_df = copy_df.drop['Store 1']
+# drop columns by using del function
+del copy_df['Name']
+copy_df
+# adding columns to the dataframe just using the assignment
+df['location']= None
+df
